407aa6cc0901df8c4b53be9340c504538606637e
ChloeSAPage <chloesapagepersonal@gmail.com>
2024-04-22 11:57:25 +0100

    adjusted font size

--- javascript/index.js
+++ javascript/index.js
@@ -1,85 +1,93 @@
 // Img Tabs
 
-const tabs = document.querySelectorAll(".tab");
-tabs.forEach(addTabListener);
+const tabs = document.querySelectorAll('.tab')
+tabs.forEach(addTabListener)
 
 function addTabListener(tab) {
-    tab.addEventListener("click", showContent);
+    tab.addEventListener('click', showContent)
 }
 
 function showContent(event) {
-    const image = document.querySelector(".tabimg-img");
-    const desc = document.querySelector(".tabimg-desc");
-    const clickedTab = event.currentTarget;
-    const tabData = clickedTab.dataset.tab;
-    console.log(tabData);
+    const image = document.querySelector('.tabimg-img')
+    const desc = document.querySelector('.tabimg-desc')
+    const clickedTab = event.currentTarget
+    const tabData = clickedTab.dataset.tab
+    console.log(tabData)
     switch (tabData) {
-        case "yellow":
+        case 'yellow':
             image.setAttribute(
-                "src",
-                "images/giorgio-trovato-fczCr7MdE7U-unsplash.jpg"
-            );
-            desc.textContent = "Bananas! How beautiful.";
-            tabs.forEach((tab) => tab.classList.remove("highlighted"));
-            clickedTab.classList.add("highlighted");
-            break;
-        case "red":
-            image.setAttribute("src", "images/redbananas2.jpg");
-            desc.textContent = "This one is red...";
-            tabs.forEach((tab) => tab.classList.remove("highlighted"));
-            clickedTab.classList.add("highlighted");
-            break;
-        case "green":
-            image.setAttribute("src", "images/greenbean.jpg");
-            desc.textContent = "Wait. That's not a banana.";
-            tabs.forEach((tab) => tab.classList.remove("highlighted"));
-            clickedTab.classList.add("highlighted");
-            break;
-        case "plantain":
-            image.setAttribute("src", "images/plantain.jpg");
-            desc.textContent = "Looks like a banana but it's not!";
-            tabs.forEach((tab) => tab.classList.remove("highlighted"));
-            clickedTab.classList.add("highlighted");
-            break;
+                'src',
+                'images/giorgio-trovato-fczCr7MdE7U-unsplash.jpg'
+            )
+            desc.textContent = 'Bananas! How beautiful.'
+            tabs.forEach((tab) => tab.classList.remove('highlighted'))
+            tabs.forEach((tab) => tab.classList.add('not-highlighted'))
+            clickedTab.classList.add('highlighted')
+            clickedTab.classList.remove('not-highlighted')
+            break
+        case 'red':
+            image.setAttribute('src', 'images/redbananas2.jpg')
+            desc.textContent = 'This one is red...'
+            tabs.forEach((tab) => tab.classList.remove('highlighted'))
+            tabs.forEach((tab) => tab.classList.add('not-highlighted'))
+            clickedTab.classList.add('highlighted')
+            clickedTab.classList.remove('not-highlighted')
+            break
+        case 'green':
+            image.setAttribute('src', 'images/greenbean.jpg')
+            desc.textContent = "Wait. That's not a banana."
+            tabs.forEach((tab) => tab.classList.remove('highlighted'))
+            tabs.forEach((tab) => tab.classList.add('not-highlighted'))
+            clickedTab.classList.add('highlighted')
+            clickedTab.classList.remove('not-highlighted')
+            break
+        case 'plantain':
+            image.setAttribute('src', 'images/plantain.jpg')
+            desc.textContent = "Looks like a banana but it's not!"
+            tabs.forEach((tab) => tab.classList.remove('highlighted'))
+            tabs.forEach((tab) => tab.classList.add('not-highlighted'))
+            clickedTab.classList.add('highlighted')
+            clickedTab.classList.remove('not-highlighted')
+            break
     }
 }
 
 // Testimonies
 
-const testimonies = document.querySelectorAll(".testimonies-person");
-testimonies.forEach(addTestimonyListener);
+const testimonies = document.querySelectorAll('.testimonies-person')
+testimonies.forEach(addTestimonyListener)
 
 function addTestimonyListener(tab) {
-    tab.addEventListener("click", showQuote);
+    tab.addEventListener('click', showQuote)
 }
 
 function showQuote(event) {
-    const quote = document.querySelector(".quote");
-    const clickedTab = event.currentTarget;
-    const tabData = clickedTab.dataset.tab;
-    console.log(tabData);
+    const quote = document.querySelector('.quote')
+    const clickedTab = event.currentTarget
+    const tabData = clickedTab.dataset.tab
+    console.log(tabData)
     switch (tabData) {
-        case "bananaman":
+        case 'bananaman':
             quote.textContent =
-                '"The biggest, yellowist, tastiest bananas I have ever eaten. Except the red one, that one was red."';
+                '"The biggest, yellowist, tastiest bananas I have ever eaten. Except the red one, that one was red."'
             testimonies.forEach((testimony) =>
-                testimony.classList.remove("selected")
-            );
-            clickedTab.classList.add("selected");
-            break;
-        case "gwen":
-            quote.textContent = '"This sh** is bananas! B-A-N-A-N-A-S!"';
+                testimony.classList.remove('selected')
+            )
+            clickedTab.classList.add('selected')
+            break
+        case 'gwen':
+            quote.textContent = '"This sh** is bananas! B-A-N-A-N-A-S!"'
             testimonies.forEach((testimony) =>
-                testimony.classList.remove("selected")
-            );
-            clickedTab.classList.add("selected");
-            break;
-        case "gorilla":
-            quote.textContent = '"Ooh Ooh Aah Aah"';
+                testimony.classList.remove('selected')
+            )
+            clickedTab.classList.add('selected')
+            break
+        case 'gorilla':
+            quote.textContent = '"Ooh Ooh Aah Aah"'
             testimonies.forEach((testimony) =>
-                testimony.classList.remove("selected")
-            );
-            clickedTab.classList.add("selected");
-            break;
+                testimony.classList.remove('selected')
+            )
+            clickedTab.classList.add('selected')
+            break
     }
 }
